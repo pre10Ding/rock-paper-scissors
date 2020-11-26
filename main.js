e3b330fde7b6740b5a9fe8aa150f8c4218c573a0
Cheng Shao <cheng.shao38@gmail.com>
2020-11-25 19:54:16 -0500

    add score tracking, texts, and game reset

--- main.js
+++ main.js
@@ -8,27 +8,32 @@
 //   }
 // }
 
-let playerSelection="";
+let playerSelection = "";
+let playerWins = 0;
+let computerWins = 0;
 
 let listOfSelection = document.querySelectorAll('.player-panel');
-listOfSelection.forEach((selected)=>selected.addEventListener('click',handleChoice));
+listOfSelection.forEach((selected) => selected.addEventListener('click', handleChoice));
 
 
 function handleChoice(e) {
 
   //remember the player choice
   playerSelection = e.path[0].id;
-  
+
   highlightChoice(e);
-  
+
   let computerSelection = computerPlay();
 
   displayComputerChoice(computerSelection);
 
   //tie=0, player win=1, cpu win=2
-  let result = playRound(playerSelection,computerSelection);
+  let result = playRound(playerSelection, computerSelection);
 
   console.log(result);
+
+  //displays corresponding text and keeps track of wins/losses
+  handleResult(result);
 
   return 0;
 }
@@ -47,7 +52,7 @@ function highlightChoice(e) {
   //a toggle to flip between flex order 1 and 3, since the player choice is order 2
   let toggle = true;
   listOfSelection.forEach((el) => {
-    if(el.classList.length===1) { //checks class length to determine if it's been processed yet
+    if (el.classList.length === 1) { //checks class length to determine if it's been processed yet
       if (toggle) {
         el.classList.add('top-panel');
         toggle = false;
@@ -55,20 +60,90 @@ function highlightChoice(e) {
       else {
         el.classList.add('bottom-panel');
       }
-    } 
-    
+    }
+
   });
 
   return 0;
 }
 
 //updates the CPU panel with the hand that CPU chose
-function displayComputerChoice(computerSelection){
+function displayComputerChoice(computerSelection) {
 
-
+  let cpuImg = document.querySelector('#cpu-icon');
+  cpuImg.setAttribute('src', `assets/${computerSelection}.png`)
 
   return 0;
 }
+
+function handleResult(result) {
+  let resultPanel = document.querySelector('#interactive-button');
+
+  switch (result) {
+    case 0:
+      resultPanel.textContent = `It's a Tie!`;
+      break;
+    case 1:
+      resultPanel.textContent = `You Win!`;
+      playerWins = playerWins + 1;
+      break;
+    case 2:
+      resultPanel.textContent = `You Lose!`;
+      computerWins = computerWins + 1;
+      break;
+  }
+
+  updateScore();
+
+  checkForGameEnd(resultPanel);
+
+
+  return 1;
+}
+
+function checkForGameEnd(resultPanel) {
+  if (playerWins >= 3 && playerWins >= computerWins) {
+    resultPanel.textContent = `Congratulations! You win with a score of 
+    ${playerWins}-${computerWins}.    
+    Click here to play again!`;
+  } else if (computerWins >= 3) {
+    resultPanel.textContent = `Oh no! You lost with a score of
+    ${playerWins}-${computerWins}.    
+    Click here to play again!`;
+  }
+  if (playerWins === 3 || computerWins === 3) {
+    let oldArea = document.querySelector('#right-side-area');
+    let newArea = oldArea.cloneNode(true);
+    oldArea.parentNode.replaceChild(newArea, oldArea);
+
+    resultPanel.addEventListener('click', resetGame)  
+  }
+}
+
+function resetGame() {
+  location.reload();
+}
+
+// function resetGame() {
+//   listOfSelection.forEach((el) => el.classList.remove(el.classList[1])); //pop the second class
+//   playerSelection = "";
+//   playerWins = 0;
+//   computerWins = 0;
+//   updateScore();
+//   displayComputerChoice("robotFace");
+//   listOfSelection.forEach((selected) => selected.addEventListener('click', handleChoice));
+
+// }
+
+function updateScore() {
+  let cpuScore = document.querySelector('#cpu-score');
+  let playerScore = document.querySelector('#player-score');
+  cpuScore.textContent = computerWins;
+  playerScore.textContent = playerWins;
+  return 1;
+}
+
+
 
 let winCount = 0;
 let lossCount = 0;
