f1083cc7e52c442a2fd33ec1785283a164eeeeed
Cheng Shao <cheng.shao38@gmail.com>
2020-11-26 17:00:29 -0500

    implement logic for changing font size of game end messages.

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ function highlightChoice(e) {
 function displayComputerChoice(computerSelection) {
 
   let cpuImg = document.querySelector('#cpu-icon');
-  cpuImg.setAttribute('src', `assets/${computerSelection}.png`)
+  cpuImg.setAttribute('src', `assets/${computerSelection}Robot.png`)
 
   return 0;
 }
@@ -103,37 +103,26 @@ function handleResult(result) {
 
 function checkForGameEnd(resultPanel) {
   if (playerWins >= 3 && playerWins >= computerWins) {
-    resultPanel.textContent = `Congratulations! You win with a score of 
-    ${playerWins}-${computerWins}.    
-    Click here to play again!`;
+    resultPanel.innerHTML = `Congratulations! <br> 
+    You win with a score of ${playerWins}-${computerWins}! <br>
+    Click here to play again.`;
   } else if (computerWins >= 3) {
-    resultPanel.textContent = `Oh no! You lost with a score of
-    ${playerWins}-${computerWins}.    
-    Click here to play again!`;
+    resultPanel.innerHTML = `Oh no! <br>
+    You lost with a score of ${playerWins}-${computerWins}! <br>
+    Click here to play again.`;
   }
   if (playerWins === 3 || computerWins === 3) {
+    resultPanel.style.fontSize = '18px';
+    resultPanel.style.lineHeight = '18px';
     let oldArea = document.querySelector('#right-side-area');
     let newArea = oldArea.cloneNode(true);
     oldArea.parentNode.replaceChild(newArea, oldArea);
 
-    resultPanel.addEventListener('click', resetGame)  
+    resultPanel.addEventListener('click', ()=>location.reload()); 
+    resultPanel.classList.add('interactive-button-button')
   }
 }
 
-function resetGame() {
-  location.reload();
-}
-
-// function resetGame() {
-//   listOfSelection.forEach((el) => el.classList.remove(el.classList[1])); //pop the second class
-//   playerSelection = "";
-//   playerWins = 0;
-//   computerWins = 0;
-//   updateScore();
-//   displayComputerChoice("robotFace");
-//   listOfSelection.forEach((selected) => selected.addEventListener('click', handleChoice));
-
-// }
 
 function updateScore() {
   let cpuScore = document.querySelector('#cpu-score');
@@ -185,72 +174,4 @@ function playRound(playerSelection, computerSelection) {
     return 2;
 
   }
-}
-
-//validate player selection, true if input is valid
-function validateSelection(playerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-  if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
-    return true;
-  }
-  else {
-    console.log(`Invalid entry`);
-    return false;
-  }
-}
-
-//update the win/loss counters in the HTML
-function updateCounters() {
-  document.getElementById("win-counter").innerHTML = winCount;
-  document.getElementById("loss-counter").innerHTML = lossCount;
-}
-
-//main game logic
-function game() {
-  let playerWins = 0;
-  let computerWins = 0;
-  let roundsLeft = 5;
-
-  for (roundsLeft; roundsLeft > 0; roundsLeft = roundsLeft - 1) {
-    let playerSelection = "";
-    //ask for and validate player selection
-    while (!validateSelection(playerSelection)) {
-      playerSelection = prompt(`Round ${playerWins + computerWins} out of 5. The score is ${playerWins}-${computerWins}. 
-Rock, paper, or scissors?`);
-    }
-    computerSelection = computerPlay();
-    //determine who is the winner
-    let result = playRound(playerSelection, computerSelection);
-    switch (result) {
-      case 0:
-        alert(`It's a Tie! We both chose ${playerSelection}`);
-        roundsLeft = roundsLeft + 1; //since it's a tie, need to increase roundsLeft by 1 to counteract the loop's decremental effect and replay this round
-        break;
-      case 1:
-        alert(`You Win! ${playerSelection} beats ${computerSelection}`)
-        playerWins = playerWins + 1;
-        break;
-      case 2:
-        alert(`You Lose! ${computerSelection} beats ${playerSelection}`)
-        computerWins = computerWins + 1;
-        break;
-    }
-    //if either reaches 3 wins, end the loop, display game end message, and call update counters.
-    if (playerWins === 3) {
-      alert(`Congratualations! You win with a score of ${playerWins}-${computerWins}`);
-      winCount = winCount + 1;
-      updateCounters();
-      return 1;
-    } else if (computerWins === 3) {
-      alert(`You lost with a score of ${playerWins}-${computerWins}`);
-      lossCount = lossCount + 1;
-      updateCounters();
-      return 1;
-    }
-  }
-  //function should have been returned before this point...
-  alert("Something went wrong. (Code 0001)");
-  return -1;
-
-
 }
